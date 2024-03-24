b15571047efb6d7269539edf0ae84a42fb2486c2
AlexeyZami <varan560045@yandex.ru>
2024-03-24 20:14:06 +0300

    создал поля класса code и path

--- src/UserInteraction.java
+++ src/UserInteraction.java
@@ -1,2 +1,12 @@
 public class UserInteraction {
+    private static int code;
+    private static String path;
+
+    public static int getCode() {
+        return code;
+    }
+
+    public static String getPath() {
+        return path;
+    }
 }
