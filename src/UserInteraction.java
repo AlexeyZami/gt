426b378e89eb979e9bf10d6e300a3a57ec955969
AlexeyZami <varan560045@yandex.ru>
2024-03-24 20:16:52 +0300

    создал метод interaction

--- src/UserInteraction.java
+++ src/UserInteraction.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class UserInteraction {
     private static int code;
     private static String path;
-    
+
     public static String createPath(){
         System.out.println("Введите путь к файлу с текстом:");
         Scanner scanner = new Scanner(System.in);
@@ -13,6 +13,17 @@ public class UserInteraction {
         System.out.println("Введите числовой код для шифрования:");
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
+    }
+    public static void interaction(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Какую операцию необходимо выполнить?");
+        System.out.println("1 - зашифровать текст\n2 - расшифровать текст\n3 - взломать шифр");
+        if (scanner.nextInt() == 1){
+            path = createPath();
+            code = createCode();
+            System.out.println("Зашифрованный текст расположен по пути:");
+            System.out.println(Encoder.writingCiphertext());
+        }
     }
 
     public static int getCode() {
