21e66ba2c3921cee2826314dccba9d6611004701
AlexeyZami <varan560045@yandex.ru>
2024-03-24 20:15:42 +0300

    создал методы createPath и createCode

--- src/UserInteraction.java
+++ src/UserInteraction.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class UserInteraction {
     private static int code;
     private static String path;
+    
+    public static String createPath(){
+        System.out.println("Введите путь к файлу с текстом:");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+    public static int createCode(){
+        System.out.println("Введите числовой код для шифрования:");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
 
     public static int getCode() {
         return code;
