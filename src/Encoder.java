29f440c8450047ce8ad0fcb806de773b952282a1
AlexeyZami <varan560045@yandex.ru>
2024-03-24 20:04:15 +0300

    создал поля inputList и encryptedList

--- src/Encoder.java
+++ src/Encoder.java
@@ -1,2 +1,15 @@
+import java.io.FileNotFoundException;
+import java.util.List;
+
 public class Encoder {
+    public static List<Integer> inputList;
+
+    static {
+        try {
+            inputList = convertToList(UserInteraction.getPath());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    public static List<Character> encryptedList = encryption(inputList);
 }
